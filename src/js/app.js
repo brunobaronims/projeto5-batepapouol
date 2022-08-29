6dd09279d7f8affd554bbcdb6f723ff2ad6ed822
Bruno Baroni <brunobaronims@gmail.com>
2022-08-29 13:37:42 -0300

    fixed chat load

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,5 @@ import * as Chat from './modules/chat.js'
 window.sendMessage = Chat.sendMessage;
 
 window.onload = (event) => {
-  Chat.loadChat('https://mock-api.driven.com.br/api/v6/uol/status');
+  Chat.load('https://mock-api.driven.com.br/api/v6/uol/status');
 };
